d1fd20745947e532f01828a63e9b8cb837593cd0
Morshues Lai <morshues@gmail.com>
2025-05-09 21:26:44 +0800

    Avoid garbled text on the filename of uploaded file

--- app/controllers/upload.controller.js
+++ app/controllers/upload.controller.js
@@ -7,5 +7,6 @@ exports.upload = async (req, res) => {
   if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
-  res.send(`${req.file.originalname} uploaded`);
+  const name = req.file.utf8Name || req.file.originalname
+  res.send(`${name} uploaded`)
 }
--- app/routes/upload.routes.js
+++ app/routes/upload.routes.js
@@ -5,7 +5,10 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
-  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
+  filename: (req, file, cb) => {
+    file.utf8Name = Buffer.from(file.originalname, 'latin1').toString('utf8')
+    cb(null, Date.now() + '-' + file.utf8Name)
+  }
 });
 const upload = multer({ storage });
 

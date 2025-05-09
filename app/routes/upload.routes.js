9d5629d0a0df4d4099a3894b8dc14f06194ec28b
Morshues Lai <morshues@gmail.com>
2025-05-09 21:28:32 +0800

    Remove date-prefix of the uploaded filename

--- app/routes/upload.routes.js
+++ app/routes/upload.routes.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
   filename: (req, file, cb) => {
     file.utf8Name = Buffer.from(file.originalname, 'latin1').toString('utf8')
-    cb(null, Date.now() + '-' + file.utf8Name)
+    cb(null, file.utf8Name)
   }
 });
 const upload = multer({ storage });
